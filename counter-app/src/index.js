1179847d943dea56fdaf40fe27ac599758f466c6
CarolinsRainbow <60187454+Carolinsrainbow@users.noreply.github.com>
2021-08-08 20:40:29 -0400

    Actualización
    Solución al error más tonto del mundo

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -1,1 +1,8 @@
-console.log("Jai Baib")
+import React from "react";
+import ReactDOM from "react-dom";
+
+const saludo = <h1>Hola Mundo</h1>;
+
+const divRoot = document.querySelector("#root");
+
+ReactDOM.render(saludo, divRoot);
